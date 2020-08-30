72bcc5af6d8b2900b86769f49668320766f9d98d
LauraKendrick <47174730+LauraKendrick@users.noreply.github.com>
2020-08-30 18:18:38 +0100

    Update header.php

--- wp-content/themes/MyCustomTheme/header.php
+++ wp-content/themes/MyCustomTheme/header.php
@@ -3,9 +3,8 @@
 
 <head>
 <meta charset=“UTF-8”>
-        <meta name=“viewport” content=“width=device-width, initial-scale=1.0”>
-        <meta http-equiv=“X-UA-Compatible” content=“ie=edge”>
-and then <link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<?php wp_head();?>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
 	<link href="style/style.css" rel="stylesheet">
