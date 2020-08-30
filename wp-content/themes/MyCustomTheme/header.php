65c492bbc781b5082c60195131ef0cb008e0463b
LauraKendrick <laurakendrick95@gmail.com>
2020-08-30 20:28:33 +0100

    Style update

--- wp-content/themes/MyCustomTheme/header.php
+++ wp-content/themes/MyCustomTheme/header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!--<link rel="icon" href="https://laurakendrick.github.io/lk-fake-html-site-wordpress/favicon.png" type="image/png" sizes="32x32">-->
+	<link rel="icon" href="https://laurakendrick.github.io/lk-fake-html-site-wordpress/favicon.png" type="image/png" sizes="32x32">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<!--link rel="icon" type="image/x-icon" href="favicon.png"  />-->
 
