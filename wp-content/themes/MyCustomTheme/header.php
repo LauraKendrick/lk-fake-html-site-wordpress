44b624c73461232ab316328fd5d3c2e9c1de1ac3
LauraKendrick <47174730+LauraKendrick@users.noreply.github.com>
2020-08-31 15:28:34 +0100

    Update header.php

--- wp-content/themes/MyCustomTheme/header.php
+++ wp-content/themes/MyCustomTheme/header.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="https://laurakendrick.github.io/lk-fake-html-site-wordpress/favicon.png" type="image/png" sizes="32x32">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<!--link rel="icon" type="image/x-icon" href="favicon.png"  />-->
